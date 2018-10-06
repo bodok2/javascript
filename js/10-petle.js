1ef858596f688a3b85e53087f0119ff5bddc9f58
Bogdan Kalinowski <bodok2@gmail.com>
2018-10-06 16:54:23 +0200

    dalej

---
+++ js/10-petle.js
@@ -0,0 +1,60 @@
+//let number = 10;
+//
+//for (let i = 0; i <= number; i++) {
+//    console.log(i);
+//}
+
+// foreach loop
+
+//let tablica = ["Ania", "Konrad", "Mateusz"];
+//
+//tablica.forEach(function(element, index) {
+//    console.log("Imie" + element + "Znajduje się pod indexem" + index);
+    
+//});
+
+//while loop
+
+
+//let number = 10;
+//let index = 0;
+//
+//while(number > 0) {
+
+//    console.log(index ++);
+//}
+
+//let number = 10;
+//
+//while(number > 0) {
+//    number--;// number = number -1;
+//    console.log(number);
+//}
+//
+
+
+//let number = 10;
+//
+//while (number > 0) {
+//    number--; // number = number -1;
+//    console.log(number)
+//    
+//    if (number === 5) {
+//        console.log("Ha wyświetla 5";);
+//    }
+//}
+  
+//objects for..in loop
+
+let adam = {
+    wiek: 10000,
+    wlosy: false,
+    oczy: true,
+    nazwisko: 'Dou'
+};
+
+for (let i in adam) {
+    console.log("Klucz :" + i + "wartość:" + adam[i]);
+}
+
+    
