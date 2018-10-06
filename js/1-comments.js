948d1113b64d68abc971e9e2ddbe088325ca4d1f
Bogdan Kalinowski <bodok2@gmail.com>
2018-10-06 09:51:22 +0200

    javascript ćwiczenia

---
+++ js/1-comments.js
@@ -0,0 +1,8 @@
+//console.log("Działą !"); - komentarz liniowy = ctrl+slash
+
+
+
+// komentarz blokowy = ctrl+shift-slash
+//
+//console.log("Działą !"),
+//console.log("Działą !"); 
---
+++ js/2-variables.js
@@ -0,0 +1,16 @@
+console.log("Działa variables");
+
+
+let imie;
+let imieMeskie = "Kacper"
+
+imie = "Monika";
+
+imie = "Gosia";
+
+imie = imieMeskie;
+
+let liczba = "5000";
+
+console.log(liczba);
+
